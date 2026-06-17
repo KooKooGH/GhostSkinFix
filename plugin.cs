c9feeac0cb0656412656c5e60e3673c5a5b3960e
KooKoo <KooKooKitty92928@gmail.com>
2026-06-17 01:30:04 -0500

    add plugin GUID

--- plugin.cs
+++ plugin.cs
@@ -5,12 +5,16 @@ using HarmonyLib;
 
 namespace GhostLeviathanSkinFix;
 
-[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
     public new static ManualLogSource Logger { get; private set; }
-    
+
     private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+    private const string PLUGIN_GUID = "com.KooKoo.GhostSkinFix";
+    private const string PLUGIN_NAME = "GhostLeviathanSkinFix";
+    private const string PLUGIN_VERSION = "1.0.0";
 
     private void Awake()
     {
@@ -18,7 +22,7 @@ public class Plugin : BaseUnityPlugin
         Logger = base.Logger;
 
         // register harmony patches, if there are any
-        Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
-        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        Harmony.CreateAndPatchAll(Assembly, PLUGIN_GUID);
+        Logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
     }
 }
