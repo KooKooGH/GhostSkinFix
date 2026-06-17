d41b00d8b65dfe7c5632bf656ad1c088adc1b386
KooKoo <KooKooKitty92928@gmail.com>
2026-06-17 02:11:55 -0500

    Why did rider auto suggest this? Remove this now

--- plugin.cs
+++ plugin.cs
@@ -5,24 +5,19 @@ using HarmonyLib;
 
 namespace GhostLeviathanSkinFix;
 
-[BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
     public new static ManualLogSource Logger { get; private set; }
 
     private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
 
-    // isn't my name so epic
-    private const string PLUGIN_GUID = "com.KooKoo.GhostSkinFix";
-    private const string PLUGIN_NAME = "GhostLeviathanSkinFix";
-    private const string PLUGIN_VERSION = "1.0.0";
-
     private void Awake()
     {
         Logger = base.Logger;
 
         // register harmony patches WOO!! Initialize!!
-        Harmony.CreateAndPatchAll(Assembly, PLUGIN_GUID);
-        Logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
+        Harmony.CreateAndPatchAll(Assembly, PluginInfo.PLUGIN_GUID);
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
     }
 }
